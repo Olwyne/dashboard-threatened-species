10f7acff114023b9080e750272d29788b6d14a2c
Mansion-Amelia <amelia.mansion@gmail.com>
2020-04-15 17:31:15 +0200

    Add fetch bloowatch

--- src/main.js
+++ src/main.js
@@ -13,22 +13,6 @@ Vue.use(BootstrapVue)
 // Install ApexCharts
 Vue.use(VueApexCharts)
 Vue.component('apexchart', VueApexCharts)
-
-// Config RedList API
-import {
-  Country,
-  //GrowthForms,
-  //Habitat,
-  //Measure,
-  //Region,
-  Species,
-  //Threat,
-} from 'iucn-red-list';
-import { setRedListToken } from 'iucn-red-list';
-setRedListToken('9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee')
-import { redListVersion } from 'iucn-red-list';
-redListVersion().then(data => console.log(data));
-
 Vue.config.productionTip = false
 
 new Vue({
@@ -38,18 +22,18 @@ new Vue({
 
 // API RedList
 
-/** Example without npm **/ 
+/*fetch('http://www.bloowatch.org/developers/json/species').then(res => res.json()).then(data => {
+    console.log(data);
+    const result = data.allSpecies
+    for(var el in result){
+        const scientific_name = result[el].scientific_name;
+        const name = result[el].name;
+        
+        fetch("http://apiv3.iucnredlist.org/api/v3/species/history/name/"+scientific_name+"?token=9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee").then(res => res.json()).then(data => {
+            console.log("History for "+name);
+            console.log(data)
+        });
+    }
+});*/
 
-/*fetch("http://apiv3.iucnredlist.org/api/v3/species/region/northern_africa/page/0?token=9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee").then(res => res.json()).then(data => console.log(data));*/
-
-/** With npm **/
-
-Species
-  .fetch({ page: 2, region: 'europe' })
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
-
-Country
-  .species({ country: 'AZ' })
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
+fetch("http://www.bloowatch.org/developers/json/species").then(res => res.json()).then(data => console.log(data));
