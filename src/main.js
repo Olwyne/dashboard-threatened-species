846254eebb21af7b55ccce8d42693ee08979b5a9
Manon Sgro <manon-sgro@outlook.fr>
2020-04-15 09:52:50 +0200

    Fetch API example

--- src/main.js
+++ src/main.js
@@ -8,6 +8,13 @@ new Vue({
 }).$mount('#app')
 
 // API function
-fetch('https://api.gbif.org/v1/species')
+fetch('https://api.gbif.org/v1/dataset/search?country=FR&facet=DECADE&limit=0')
   .then(response => response.json())
-  .then(data => console.log(JSON.stringify(data)))
+  .then(data => {
+    console.log("Nombre d'espèces menacées depuis 1920 :");
+    const result = data.facets[0].counts; // tableau date-nb d'espèces
+    
+    for(var el in result.sort((a, b) => (a.name > b.name) ? 1 : -1)){ // tri par date croissante
+        console.log("En "+result[el].name+" : "+result[el].count+" espèces.");
+    }
+})
