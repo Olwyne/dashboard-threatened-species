0aaba2e9000cad7c2975e3f12300866bf801038f
Laura <laura.dietsch98@gmail.com>
2020-04-15 15:44:05 +0200

    add Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Navbar />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import { mapActions, mapGetters } from 'vuex'
+    import Navbar from './components/Navbar.vue'
+    import { mapActions, mapGetters } from 'vuex'
 
-export default {
-	name: 'App',
-	components: {
-		HelloWorld
-	},
-	data: function () {
-        return {
-        }
-    },
-    methods: {
-        ... mapActions([
+
+    export default {
+        name: 'App',
+        components: {
+            Navbar
+        },
+        data: function () {
+            return {
+            }
+        },
+        methods: {
+            ...mapActions([
                 'addLocationToStore',
                 'setActivePage',
                 'addWalkToStore',
                 'setActiveTitle'
-        ]),
-    },
-    computed:{
-            ... mapGetters([
+            ]),
+        },
+        computed: {
+            ...mapGetters([
                 'getActivePage',
                 'getActiveTitle',
                 'getGameState',
                 'getPreviousPage'
             ]),
-    },
-	mounted: function(){
+        },
+        mounted: function () {
+        }
     }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+    }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,23 @@
 <template>
-    <div class="navigation">
-       <div>ma navbar</div>
+    <div class="navigation" >
+                <b-nav  align="center" class="bg-danger myNav">
+                    <!-- Those are animal filers, the user can select none, one or several of them, so several animals can be "active"-->
+                    <!-- The name of the animals must be replaced by their icons from the API -->
+                    <b-nav-item>Panda</b-nav-item>
+                    <b-nav-item>Monkey</b-nav-item>
+                    <b-nav-item>Gorilla </b-nav-item>
+                    <b-nav-item>Panthera</b-nav-item>
+                    <b-nav-item>Panda</b-nav-item>
+                    <b-nav-item>Monkey</b-nav-item>
+                    <b-nav-item>Gorilla </b-nav-item>
+                    <b-nav-item>Panthera</b-nav-item>
+                    <b-nav-item>Panda</b-nav-item>
+                    <b-nav-item>Monkey</b-nav-item>
+                    <b-nav-item>Gorilla </b-nav-item>
+                    <b-nav-item>Panthera</b-nav-item>
+                </b-nav>
     </div>
 </template>
-
 <script>
 
     export default {
@@ -27,5 +41,8 @@
 </script>
 
 <style>
+    .myNav a {
+        color: white;
+    }
 
 </style>
