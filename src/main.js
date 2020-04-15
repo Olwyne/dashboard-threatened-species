7c08d7803afd91073f48ecc2197ec88b0c222685
Manon Sgro <manon-sgro@outlook.fr>
2020-04-15 09:30:25 +0200

    Fetch API

--- src/main.js
+++ src/main.js
@@ -6,3 +6,8 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+// API function
+fetch('https://api.gbif.org/v1/species')
+  .then(response => response.json())
+  .then(data => console.log(JSON.stringify(data)))
