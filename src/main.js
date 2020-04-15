79996cce02dc6b55adcc9cf1e4319c8d75015555
Manon Sgro <manon-sgro@outlook.fr>
2020-04-15 12:06:55 +0200

    Fetch API (RedList)

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ new Vue({
   render: h => h(App),
 }).$mount('#app')
 
-// API function
+// API GBIF
 fetch('https://api.gbif.org/v1/dataset/search?country=FR&facet=DECADE&limit=0')
   .then(response => response.json())
   .then(data => {
@@ -18,3 +18,58 @@ fetch('https://api.gbif.org/v1/dataset/search?country=FR&facet=DECADE&limit=0')
         console.log("En "+result[el].name+" : "+result[el].count+" espèces.");
     }
 })
+
+fetch('https://api.gbif.org/v1/species/search?isExtinct=true')
+  .then(response => response.json())
+  .then(data => {
+    console.log("Espèces éteintes :");
+    const result = data.results; // tableau espèces
+    
+    for(var el in result){
+        console.log(result[el].scientificName);
+    }
+})
+
+fetch("http://apiv3.iucnredlist.org/api/v3/species/region/northern_africa/page/0?token=9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee").then(res => res.json()).then(data => console.log(data));
+
+// API Bloowatch
+
+/*async function getData() {
+      try {
+          const res = await fetch('http://bloowatch.org/developers/json/species');
+          const data = await res.json();
+          console.log(data);
+      } catch(err) {
+          console.log(err);
+      }
+  }
+console.log("ok");
+  getData();
+fetch('http://bloowatch.org/developers/json/species')
+      .then(res => res.json())
+      .then(jsonRes => console.log(jsonRes))
+      .catch(error => console.log(error));*/
+
+/*fetch('http://bloowatch.org/developers/json/species', {
+  headers: { "Content-Type": "application/json" },
+  method: 'GET'
+})*/
+
+/*async function getData() {
+      try {
+          const res = fetch('http://localhost:8080/developers/json/species/giant-panda');
+          //const data = res.json();
+          //console.log(res.arrayBuffer());
+          console.log(res.json());
+      } catch(err) {
+          console.log(err);
+      }
+  }
+  getData();*/
+  /*fetch('http://localhost:8080/developers/json/species')
+      .then(res => res.json())
+      .then(jsonRes => console.log(jsonRes))
+      .catch(error => console.log(error));*/
+fetch('http://localhost:8080/developers/json/species')
+  .then(response => console.log(response))
+  .then(data => console.log(data))
