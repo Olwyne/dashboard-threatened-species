bc1dfec12dde089deb21c8eee4dd7c202db29860
Laura <laura.dietsch98@gmail.com>
2020-04-15 14:45:45 +0200

    start creating a navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="navigation">
+       <div>ma navbar</div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+	name: 'Navbar',
+	props: {
+		msg: String
+	},
+	data: function () {
+        return {
+        }
+    },
+    methods: {
+
+    },
+    computed:{
+
+    },
+	mounted: function(){
+    }
+}
+</script>
+
+<style>
+
+</style>
